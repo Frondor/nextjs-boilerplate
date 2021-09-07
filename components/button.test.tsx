d3562aea7df5a7d676f6e14ab5f18486ad90787c
Federico Vázquez <federico.vazquez@mercadolibre.com>
2021-09-07 14:53:34 -0300

    Add button.test

--- components/button.test.tsx
+++ components/button.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './button'
 
 describe('home', () => {
-  it('renders a button with its children', () => {
-    render(<Button>Click me!</Button>)
+  it('renders a button', () => {
+    const onClick = jest.fn()
+    render(<Button onClick={onClick}>Click me!</Button>)
 
+    const btn = screen.getByText(/click me/i)
     expect(screen.getByText(/click me/i)).toBeInTheDocument()
+
+    fireEvent.click(btn)
+    expect(onClick).toHaveBeenCalledTimes(1)
   })
 })
