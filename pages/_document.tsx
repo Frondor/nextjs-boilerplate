d0c469af51ebb6272ade8bc7609315b9d1a1511c
Federico Vázquez <federico.vazquez@mercadolibre.com>
2021-09-07 06:41:33 -0300

    Add custom font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,11 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
